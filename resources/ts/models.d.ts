7a648354a29fb13709057463f0e4ee22ca74fccb
Theofilos Intzoglou <int.teo@gmail.com>
2023-12-12 11:58:27 +0000

    Ορισμός μοντέλων

---
+++ resources/ts/models.d.ts
@@ -0,0 +1,27 @@
+declare namespace App.Models {
+    export interface User {
+        id: number;
+        name: string;
+    }
+
+    export interface Calendar {
+        id: number;
+        name: string;
+        active: boolean;
+        created_at: string | null;
+        updated_at: string | null;
+    }
+
+    export interface CalendarEvent {
+        id: number;
+        title: string;
+        description: string;
+        start_date: string;
+        end_date: string;
+        location: string;
+        url: string;
+        calendar_id: number;
+        created_at: string | null;
+        updated_at: string | null;
+    }
+}
