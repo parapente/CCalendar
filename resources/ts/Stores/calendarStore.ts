f1a5d907fe6a531efd2f7aaf3017ed7f99aa670f
Theofilos Intzoglou <int.teo@gmail.com>
2023-12-13 13:48:21 +0000

    Αλλαγή ονόματος store

--- resources/ts/Stores/calendarStore.ts
+++ resources/ts/Stores/calendarStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 
-export const useCalendarStore = defineStore("wizard", {
+export const useCalendarStore = defineStore("calendar", {
     state: (): {
         calendars: Array<App.Models.Calendar>;
         calendarEvents: Array<App.Models.CalendarEvent>;
