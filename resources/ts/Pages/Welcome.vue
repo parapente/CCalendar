4795f31e66bb5cd6795722d67a00d394180201f3
Theofilos Intzoglou <int.teo@gmail.com>
2023-12-15 12:52:49 +0000

    Διόρθωση αρχικής σελίδας

--- resources/ts/Pages/Welcome.vue
+++ resources/ts/Pages/Welcome.vue
@@ -10,9 +10,11 @@ import route from "ziggy";
         class="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8 bg-white text-black dark:bg-gray-900 dark:text-gray-100"
     >
         <div class="flex flex-col text-center">
-            <div>Καλωσορίσατε στην εφαρμογή δικτυακών ημερολογίων</div>
+            <div class="text-4xl mb-16">
+                Καλωσορίσατε στην εφαρμογή δικτυακών ημερολογίων
+            </div>
             <a
-                :href="route('calendar')"
+                :href="route('calendar.index')"
                 class="text-blue-500 my-4 py-2 px-4 rounded-md bg-blue-900 hover:bg-blue-600 hover:text-blue-300"
             >
                 Είσοδος
