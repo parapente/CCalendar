4ea908737cb92bbe604fa86675708fbf28eea731
Theofilos Intzoglou <int.teo@gmail.com>
2024-01-08 10:05:13 +0000

    Δημιουργία αρχικής σελίδας για τον διαχειριστή

--- resources/ts/Pages/Admin/Dashboard.vue
+++ resources/ts/Pages/Admin/Dashboard.vue
@@ -1,6 +1,13 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import Welcome from "@/Components/Welcome.vue";
+import {
+    faCalendarAlt,
+    faEye,
+    faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { Link } from "@inertiajs/vue3";
+import route from "ziggy";
 </script>
 
 <template>
@@ -16,9 +23,44 @@ import Welcome from "@/Components/Welcome.vue";
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div
-                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg dark:text-white p-4"
                 >
-                    <Welcome />
+                    <div>
+                        Καλωσορίσατε στην εφαρμογή δικτυακών ημερολογίων.
+                        Παρακαλώ επιλέξτε μια από τις παρακάτω λειτουργίες:
+                    </div>
+                    <div class="flex gap-4 my-4">
+                        <Link
+                            :href="route('administrator.user.index')"
+                            class="border rounded-lg bg-sky-400 p-4 shadow-lg dark:shadow-black dark:border-black"
+                        >
+                            <div class="flex flex-col text-center text-black">
+                                <FontAwesomeIcon :icon="faUsers" size="5x" />
+                                <div class="mt-3">Διαχείριση χρηστών</div>
+                            </div></Link
+                        >
+                        <Link
+                            :href="route('administrator.calendar.index')"
+                            class="border rounded-lg bg-sky-400 p-4 shadow-lg dark:shadow-black dark:border-black"
+                        >
+                            <div class="flex flex-col text-center text-black">
+                                <FontAwesomeIcon
+                                    :icon="faCalendarAlt"
+                                    size="5x"
+                                />
+                                <div class="mt-3">Διαχείριση ημερολογίων</div>
+                            </div></Link
+                        >
+                        <Link
+                            :href="route('administrator.calendar.overview')"
+                            class="border rounded-lg bg-sky-400 p-4 shadow-lg dark:shadow-black dark:border-black"
+                        >
+                            <div class="flex flex-col text-center text-black">
+                                <FontAwesomeIcon :icon="faEye" size="5x" />
+                                <div class="mt-3">Εποπτεία ημερολογίων</div>
+                            </div></Link
+                        >
+                    </div>
                 </div>
             </div>
         </div>
