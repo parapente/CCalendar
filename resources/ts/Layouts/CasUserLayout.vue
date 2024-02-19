10979814b2570278cab9c8b4767044b371d09614
Theofilos Intzoglou <int.teo@gmail.com>
2024-02-19 14:24:19 +0000

    Προσθήκη ελέγχου για δημιουργία νέας εκδήλωσης

--- resources/ts/Layouts/CasUserLayout.vue
+++ resources/ts/Layouts/CasUserLayout.vue
@@ -58,6 +58,7 @@ const page = usePage<PageWithSharedProps>();
                                             <button
                                                 type="button"
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
+                                                test-data-id="user-menu"
                                             >
                                                 {{ page.props.cas_user!.name }}
 
@@ -84,6 +85,7 @@ const page = usePage<PageWithSharedProps>();
                                         <DropdownLink
                                             as="a"
                                             :href="route('cas.logout')"
+                                            test-data-id="user-logout"
                                         >
                                             Αποσύνδεση
                                         </DropdownLink>
