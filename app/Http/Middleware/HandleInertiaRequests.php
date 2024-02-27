e7d2da6989e232fbe3f02ff535b8996e6cb74ca1
Theofilos Intzoglou <int.teo@gmail.com>
2024-02-27 11:10:28 +0000

    Δυνατότητα προσθήκης οργανισμού στην αρχική σελίδα Fixes #15

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'organization' => env('APP_ORGANIZATION'),
         ]);
     }
 }
--- resources/ts/Pages/Welcome.vue
+++ resources/ts/Pages/Welcome.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { Head } from "@inertiajs/vue3";
+import type { PageWithSharedProps } from "@/pageprops";
+import { Head, usePage } from "@inertiajs/vue3";
 import route from "ziggy";
+
+const page = usePage<PageWithSharedProps>();
 </script>
 
 <template>
@@ -10,12 +13,15 @@ import route from "ziggy";
         class="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8 bg-white text-black dark:bg-gray-900 dark:text-gray-100"
     >
         <div class="flex flex-col text-center">
+            <div class="text-6xl mb-16">
+                {{ page.props.organization }}
+            </div>
             <div class="text-4xl mb-16">
                 Καλωσορίσατε στην εφαρμογή δικτυακών ημερολογίων
             </div>
             <a
                 :href="route('calendar.index')"
-                class="text-blue-500 my-4 py-2 px-4 rounded-md bg-blue-900 hover:bg-blue-600 hover:text-blue-300"
+                class="text-blue-500 my-4 py-2 px-4 rounded-md bg-blue-900 hover:bg-blue-600 hover:text-blue-300 text-2xl"
                 test-data-id="login"
             >
                 Είσοδος
