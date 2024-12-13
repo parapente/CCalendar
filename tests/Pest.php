7bd0b823a4474f2eacd5f58e571f04611ca26bf9
Władysław Mostowicz <m0stwan1ed@gmail.com>
2024-12-13 17:34:07 +0100

    Added missing module import

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Contracts\CasAuthInterface;
+use App\Services\TestCasAuthService;
 use App\Models\CasUser;
 use App\Models\Role;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
